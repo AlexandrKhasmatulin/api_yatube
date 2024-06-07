d5d4a754650fe0287e9640642142683dbe6c5ef9
AlexandrKhasmatulin <alexandr.khasmatulin@yandex.ru>
2024-06-07 20:13:47 +0300

    final

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -19,7 +19,7 @@ class PostViewSet(viewsets.ModelViewSet):
 
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+    permission_classes = (permissions.IsAuthenticated, IsAuthorOrReadOnly)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -28,7 +28,7 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
 
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+    permission_classes = (permissions.IsAuthenticated, IsAuthorOrReadOnly)
 
     def get_post(self):
         return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
